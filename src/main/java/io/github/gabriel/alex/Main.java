a4a636c45f0a11a6aeb2d7be6ae7dc54a7b87d3d
AlexGab <alex(pt)tr(pt)gabriel(at)gmail(pt)com>
2018-07-05 18:52:22 +0200

    add tool section
    Done
    - list of techniques
    - list of workshop
    to do
    - list of strategy
    - add workshop
    - add and modify strategy
    bug
    - detail of a workshop

--- src/main/java/io/github/gabriel/alex/Main.java
+++ src/main/java/io/github/gabriel/alex/Main.java
@@ -15,79 +15,185 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
+/**
+ * Main controller of the web site
+ * 
+ * @author Alex Gabriel
+ * @version 1.0
+ * @since 1.0
+ */
 @Controller
 @SpringBootApplication
 public class Main {
-	
-	@Value("${spring.datasource.url}")
-	  private String dbUrl;
 
-	  @Autowired
-	  private DataSource dataSource;
+	@Value("${spring.datasource.url}")
+	private String dbUrl;
+
+	@Autowired
+	private DataSource dataSource;
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(Main.class, args);
 
 	}
-	
-	@RequestMapping("/")
-	  String index() {
-	    return "index";
-	  }
 
-	  @RequestMapping("/db")
-	  String db(Map<String, Object> model) {
-	    try (Connection connection = dataSource.getConnection()) {
-	      Statement stmt = connection.createStatement();
-	      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-	      stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-	      ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+	/**
+	 * Mapping for the index page
+	 * 
+	 * @return index page
+	 */
+	@RequestMapping("/index")
+	String index() {
+		return "index";
+	}
 
-	      ArrayList<String> output = new ArrayList<String>();
-	      while (rs.next()) {
-	        output.add("Read from DB: " + rs.getTimestamp("tick"));
-	      }
+	/**
+	 * Mapping for accessing example of a DB page
+	 * 
+	 * @param model
+	 * @return html page db
+	 */
+	@RequestMapping("/db")
+	String db(Map<String, Object> model) {
+		try (Connection connection = dataSource.getConnection()) {
+			Statement stmt = connection.createStatement();
+			stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
+			stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+			ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
 
-	      model.put("records", output);
-	      return "db";
-	    } catch (Exception e) {
-	      model.put("message", e.getMessage());
-	      return "error";
-	    }
-	  }
-	  
-	  @RequestMapping("/test")
-	  String test(Map<String, Object> model) {
-		  
-		  ArrayList<String> instances = new ArrayList<String>();
+			ArrayList<String> output = new ArrayList<String>();
+			while (rs.next()) {
+				output.add("Read from DB: " + rs.getTimestamp("tick"));
+			}
 
-		  OntologyManager ontoMngr = new OntologyManager();
-		  try {
-			ontoMngr.InitOntology();
-			instances = ontoMngr.getInstances();
+			model.put("records", output);
+			return "db";
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			model.put("message", e.getMessage());
+			return "error";
 		}
-		  model.put("instances", instances);
-		  return "test";
-	  }
+	}
 
-	  @Bean
-	  public DataSource dataSource() throws SQLException {
-	    if (dbUrl == null || dbUrl.isEmpty()) {
-	      return new HikariDataSource();
-	    } else {
-	      HikariConfig config = new HikariConfig();
-	      config.setJdbcUrl(dbUrl);
-	      return new HikariDataSource(config);
-	    }
-	  }
+	/**
+	 * Mapping for accessing creative technique
+	 * 
+	 * @param model
+	 * @return html page techniques
+	 */
+	@RequestMapping("/techniques")
+	String techniques(Map<String, Object> model) {
+		ArrayList<Workshop> workshops = new ArrayList<Workshop>();
+		ArrayList<CreativeTechnique> techniques = new ArrayList<CreativeTechnique>();
+
+		OntologyManager ontoMngr = new OntologyManager();
+		techniques = ontoMngr.getTechniques();
+		workshops = ontoMngr.getWorkshops();
+		
+		model.put("techniques", techniques);
+		model.put("workshops", workshops);
+		
+		return "techniques";
+	}
+
+	/**
+	 * Mapping for accessing detail of a creative technique
+	 * 
+	 * @param uri
+	 *            short uri (after #) of the creative technique
+	 * @param model
+	 * @return html page "techniqueDetail.html"
+	 */
+	@RequestMapping(value = "/techniqueDetail", method = RequestMethod.GET)
+	public String techniqueDetail(@RequestParam String uri, Map<String, Object> model) {
+		CreativeTechnique technique = new CreativeTechnique();
+		OntologyManager ontoMngr = new OntologyManager();
+		technique = ontoMngr.getTechnique(uri);
+		model.put("technique", technique);
+		return "techniqueDetail";
+	}
+	
+	/**
+	 * Mapping for accessing to the page that define the creative strategy
+	 * @param model
+	 * @return html page "creative-strategy.html"
+	 */
+	@RequestMapping(value="/creative-strategy", method = RequestMethod.GET)
+	String test(Map<String, Object> model) {
+		ArrayList<Workshop> workshops = new ArrayList<Workshop>();
+
+		OntologyManager ontoMngr = new OntologyManager();
+
+		workshops =ontoMngr.getWorkshops();
+
+		model.put("workshops", workshops);
+		return "creativeStrategy";
+	}
+	
+	
+	/**
+	 * 
+	 * @param model
+	 * @return
+	 */
+	@PostMapping("/workhop/create/")
+	String strategyForm(@ModelAttribute Workshop wrkshp, Map<String, Object> model) {
+		ArrayList<Workshop> workshops = new ArrayList<Workshop>();
+		OntologyManager ontoMngr = new OntologyManager();
+		ontoMngr.setWorkshop(wrkshp);
+		workshops =ontoMngr.getWorkshops();
+		model.put("workshops", workshops);
+		return "creativeStrategy";
+	}
+	
+	@GetMapping("/workshop/create/")
+	String createStrategy(Model model, Map<String, Object> map) {
+		model.addAttribute("workshop", new Workshop());
+		ArrayList<Workshop> workshops = new ArrayList<Workshop>();
+		OntologyManager ontoMngr = new OntologyManager();
+		workshops = ontoMngr.getWorkshops();
+		map.put("workshops", workshops);
+		return "createWorkshop";
+	}
+	
+	@RequestMapping(value = "/workshop", method = RequestMethod.GET)
+	public String workshopDetail(@RequestParam String uri, Map<String, Object> model) {
+		Workshop workshop = new Workshop();
+		ArrayList<Workshop> workshops = new ArrayList<Workshop>();
+		OntologyManager ontoMngr = new OntologyManager();
+		workshops = ontoMngr.getWorkshops();
+		workshop = ontoMngr.getWorkshop(uri);
+		model.put("workshop", workshop);
+		model.put("workshops", workshops);
+		return "workshop";
+	}
+	
+
+	/**
+	 * Beans for generating a datasource needed to access to a DB
+	 * 
+	 * @return datasource needed to establish a connection the DB
+	 * @throws SQLException
+	 */
+	@Bean
+	public DataSource dataSource() throws SQLException {
+		if (dbUrl == null || dbUrl.isEmpty()) {
+			return new HikariDataSource();
+		} else {
+			HikariConfig config = new HikariConfig();
+			config.setJdbcUrl(dbUrl);
+			return new HikariDataSource(config);
+		}
+	}
 
 }
--- src/main/java/io/github/gabriel/alex/OntologyManager.java
+++ src/main/java/io/github/gabriel/alex/OntologyManager.java
@@ -7,7 +7,9 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
+import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.dlsyntax.renderer.DLSyntaxObjectRenderer;
@@ -15,52 +17,270 @@ import org.semanticweb.owlapi.formats.PrefixDocumentFormat;
 import org.semanticweb.owlapi.io.OWLObjectRenderer;
 import org.semanticweb.owlapi.model.IRI;
 import org.semanticweb.owlapi.model.OWLAnnotation;
+import org.semanticweb.owlapi.model.OWLAnnotationProperty;
 import org.semanticweb.owlapi.model.OWLClass;
 import org.semanticweb.owlapi.model.OWLDataFactory;
+import org.semanticweb.owlapi.model.OWLDataProperty;
+import org.semanticweb.owlapi.model.OWLEntity;
 import org.semanticweb.owlapi.model.OWLLiteral;
 import org.semanticweb.owlapi.model.OWLLogicalAxiom;
 import org.semanticweb.owlapi.model.OWLNamedIndividual;
+import org.semanticweb.owlapi.model.OWLObjectProperty;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
 import org.semanticweb.owlapi.model.PrefixManager;
+import org.semanticweb.owlapi.reasoner.NodeSet;
 import org.semanticweb.owlapi.reasoner.OWLReasoner;
 import org.semanticweb.owlapi.reasoner.OWLReasonerFactory;
 import org.semanticweb.owlapi.reasoner.SimpleConfiguration;
+import org.semanticweb.owlapi.reasoner.structural.StructuralReasonerFactory;
 import org.semanticweb.owlapi.search.EntitySearcher;
 import org.semanticweb.owlapi.util.DefaultPrefixManager;
 import org.semanticweb.owlapi.vocab.OWLRDFVocabulary;
 
+/**
+ * class to manipulate the CWMO ontology
+ * 
+ * @author Alex Gabriel
+ * @version 1.0
+ * @since 1.0
+ */
 public class OntologyManager {
-	//private static final String FILE = "CWMO-05.owl";
-	OWLOntology onto;
-	
-	public void InitOntology() {
-	//public void InitOntology() {
-		
-		OWLOntologyManager man = OWLManager.createOWLOntologyManager();
-		//File fileout = new File("C:\\pizza.func.owl");
-		IRI pizzaontology = IRI.create("https://raw.githubusercontent.com/gabriel-alex/cwmo/master/CWMO-05.owl");
+	// private static final String FILE = "CWMO-05.owl";
+	/**
+	 * Ontology manager of the OWL API
+	 */
+	private OWLOntologyManager man;
+	/**
+	 * Reference to the loaded ontology
+	 */
+	private OWLOntology onto;
+
+	/**
+	 * Constructor OntologyManager - load the cwmo ontology available on github
+	 * 
+	 */
+	OntologyManager() {
+		IRI cwmo = IRI.create("https://raw.githubusercontent.com/gabriel-alex/cwmo/master/CWMO-05.owl");
+		man = OWLManager.createOWLOntologyManager();
 		try {
-			onto = man.loadOntology(pizzaontology);
+			onto = man.loadOntology(cwmo);
 		} catch (OWLOntologyCreationException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-        
-        
 	}
-	
-	public ArrayList<String> getInstances(){
-		//ArrayList<OWLLogicalAxiom> output = new ArrayList<OWLLogicalAxiom>();
+
+	/**
+	 * Constructor OntologyManager - load the ontology given as parameter
+	 * 
+	 * @param ontologyUrl
+	 *            Url to ontology available online that will be loaded
+	 */
+	OntologyManager(String ontologyUrl) {
+		IRI cwmo = IRI.create(ontologyUrl);
+		man = OWLManager.createOWLOntologyManager();
+		try {
+			onto = man.loadOntology(cwmo);
+		} catch (OWLOntologyCreationException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public ArrayList<String> getInstances() {
+		// ArrayList<OWLLogicalAxiom> output = new ArrayList<OWLLogicalAxiom>();
 		ArrayList<String> output = new ArrayList<String>();
-		//get class and its individuals
-		//onto.logicalAxioms().forEach(output::add);
-		onto.logicalAxioms().map(x->x.toString()).forEach(output::add);
-		
+		// get class and its individuals
+		// onto.logicalAxioms().forEach(output::add);
+		onto.logicalAxioms().map(x -> x.toString()).forEach(output::add);
+
 		return output;
 	}
+
+	/**
+	 * Method that creates a creative technique object based on the information
+	 * available in the ontology
+	 * 
+	 * @param shortURI
+	 *            URI of a creative technique available in the ontology
+	 * @return CreativeTechnique Object that contain all the information concerning
+	 *         the creative technique specified by the URI
+	 * 
+	 */
+	public CreativeTechnique getTechnique(String shortURI) {
+		CreativeTechnique tech = new CreativeTechnique(shortURI);
+		OWLReasonerFactory reasonerFactory = new StructuralReasonerFactory();
+		OWLReasoner reasoner = reasonerFactory.createReasoner(onto);
+
+		IRI techIRI = IRI.create("http://purl.org/cwmo/#" + shortURI);
+		OWLDataFactory dataFact = man.getOWLDataFactory();
+		OWLNamedIndividual creatTechInstances = dataFact.getOWLNamedIndividual(techIRI);
+
+		OWLAnnotationProperty rdfLabel = dataFact.getOWLAnnotationProperty(OWLRDFVocabulary.RDFS_LABEL.getIRI());
+		/*
+		 * Stream<OWLAnnotation> labelAnnotation =
+		 * EntitySearcher.getAnnotations(creatTechInstances, onto, rdfLabel);
+		 * labelAnnotation.forEach(label -> { // IRI labelIRI = label.asIRI();
+		 * OWLLiteral labelLiteral =
+		 * dataFact.getOWLLiteral(label.getValue().toString());
+		 * tech.setName(labelLiteral.getLiteral()); // OWLAnnotationValue val =
+		 * dataFact.getOWLAnnotationProperty(labelIRI); //
+		 * tech.setDifficulty(label.getValue()); });
+		 */
+
+		OWLDataProperty title = dataFact.getOWLDataProperty("http://purl.org/cwmo/#title");
+		Set<OWLLiteral> techTitle = reasoner.getDataPropertyValues(creatTechInstances, title);
+		techTitle.forEach(name -> tech.setName(name.toString()));
+
+		OWLDataProperty description = dataFact.getOWLDataProperty("http://purl.org/cwmo/#description");
+		Set<OWLLiteral> techDescription = reasoner.getDataPropertyValues(creatTechInstances, description);
+		techDescription.forEach(desc -> tech.setDescription(desc.toString()));
+
+		OWLObjectProperty hasDifficulty = dataFact.getOWLObjectProperty("http://purl.org/cwmo/#hasDifficulty");
+		Stream<OWLNamedIndividual> difficulty = reasoner.getObjectPropertyValues(creatTechInstances, hasDifficulty)
+				.entities();
+		difficulty.forEach(dif -> {
+			Stream<OWLAnnotation> difLabelAnnot = EntitySearcher.getAnnotations(dif.getIRI(), onto, rdfLabel);
+			difLabelAnnot.forEach(label -> {
+				// IRI labelIRI = label.asIRI();
+				OWLLiteral labelLiteral = dataFact.getOWLLiteral(label.getValue().toString());
+				tech.setDifficulty(labelLiteral.getLiteral());
+				// OWLAnnotationValue val = dataFact.getOWLAnnotationProperty(labelIRI);
+				// tech.setDifficulty(label.getValue());
+			});
+		});
+
+		OWLObjectProperty hasEnergyLevel = dataFact.getOWLObjectProperty("http://purl.org/cwmo/#hasEnergyLevel");
+		Stream<OWLNamedIndividual> energyLevel = reasoner.getObjectPropertyValues(creatTechInstances, hasEnergyLevel)
+				.entities();
+		energyLevel.forEach(energy -> tech.setEnergyLevel(energy.getIRI().getShortForm()));
+
+		OWLObjectProperty hasQuantityLevel = dataFact.getOWLObjectProperty("http://purl.org/cwmo/#hasQuantityLevel");
+		Stream<OWLNamedIndividual> quantityLevel = reasoner
+				.getObjectPropertyValues(creatTechInstances, hasQuantityLevel).entities();
+		quantityLevel.forEach(quantity -> tech.setQuantityLevel(quantity.getIRI().getShortForm()));
+
+		OWLObjectProperty hasNoveltyLevel = dataFact.getOWLObjectProperty("http://purl.org/cwmo/#hasNoveltyLevel");
+		Stream<OWLNamedIndividual> noveltyLevel = reasoner.getObjectPropertyValues(creatTechInstances, hasNoveltyLevel)
+				.entities();
+		noveltyLevel.forEach(novelty -> tech.setNovelty(novelty.getIRI().getShortForm()));
+
+		OWLObjectProperty hasFunFactor = dataFact.getOWLObjectProperty("http://purl.org/cwmo/#hasFunFactor");
+		Stream<OWLNamedIndividual> funFactor = reasoner.getObjectPropertyValues(creatTechInstances, hasFunFactor)
+				.entities();
+		funFactor.forEach(fun -> tech.setFunFactor(fun.getIRI().getShortForm()));
+
+		/*
+		 * OWLObjectProperty requireStimuliType =
+		 * dataFact.getOWLObjectProperty("http://purl.org/cwmo/#requireStimuliType");
+		 * Stream<OWLNamedIndividual> stimuliType =
+		 * reasoner.getObjectPropertyValues(creatTechInstances,requireStimuliType).
+		 * entities();
+		 * stimuliType.forEach(stimuli->tech.setStimuliType(stimuli.getIRI().
+		 * getShortForm()));
+		 */
+
+		OWLObjectProperty hasCreativePhase = dataFact.getOWLObjectProperty("http://purl.org/cwmo/#hasCreativePhase");
+		Stream<OWLNamedIndividual> creatPhase = reasoner.getObjectPropertyValues(creatTechInstances, hasCreativePhase)
+				.entities();
+		creatPhase.forEach(phase -> tech.setCreativePhase(phase.getIRI().getShortForm()));
+
+		OWLObjectProperty hasFunction = dataFact.getOWLObjectProperty("http://purl.org/cwmo/#hasFunction");
+		Stream<OWLNamedIndividual> function = reasoner.getObjectPropertyValues(creatTechInstances, hasFunction)
+				.entities();
+		function.forEach(phase -> tech.setFunction(phase.getIRI().getShortForm()));
+
+		return tech;
+	}
+
+	/**
+	 * Create a list of creative technique that are available in the ontology
+	 * 
+	 * @return ArrayList of Creative technique
+	 */
+	public ArrayList<CreativeTechnique> getTechniques() {
+		ArrayList<CreativeTechnique> ctList = new ArrayList<CreativeTechnique>();
+		OWLReasonerFactory reasonerFactory = new StructuralReasonerFactory();
+		OWLReasoner reasoner = reasonerFactory.createReasoner(onto);
+
+		IRI creatTechnique = IRI.create("http://purl.org/cwmo/#CreativeTechnique");
+		OWLDataFactory dataFact = man.getOWLDataFactory();
+		OWLClass CreatTechClass = dataFact.getOWLClass(creatTechnique);
+
+		NodeSet<OWLNamedIndividual> individualsNodeSet = reasoner.getInstances(CreatTechClass, true);
+		Stream<OWLNamedIndividual> individuals = individualsNodeSet.entities();
+
+		individuals.forEach(x -> ctList.add(getTechnique(x.getIRI().getShortForm())));
+
+		return ctList;
+	}
 	
+	/**
+	 * look up and create an instance of workshop thanks to the short URI
+	 * @param shortURI 
+	 * @return Workshop - object that describes workshop
+	 */
+	public Workshop getWorkshop(String shortURI){
+		Workshop wrkshp = new Workshop(shortURI);
+		OWLReasonerFactory reasonerFactory = new StructuralReasonerFactory();
+		OWLReasoner reasoner = reasonerFactory.createReasoner(onto);
+
+		IRI wrkshpIRI = IRI.create("http://purl.org/cwmo/#" + shortURI);
+		OWLDataFactory dataFact = man.getOWLDataFactory();
+		OWLNamedIndividual workshopInstance = dataFact.getOWLNamedIndividual(wrkshpIRI);
+		
+		OWLDataProperty title = dataFact.getOWLDataProperty("http://purl.org/cwmo/#title");
+		Set<OWLLiteral> wrkshpTitle = reasoner.getDataPropertyValues(workshopInstance, title);
+		wrkshpTitle.forEach(name -> wrkshp.setTitle(name.toString()));
+
+		OWLDataProperty description = dataFact.getOWLDataProperty("http://purl.org/cwmo/#description");
+		Set<OWLLiteral> wrkshpDescription = reasoner.getDataPropertyValues(workshopInstance, description);
+		wrkshpDescription.forEach(desc -> wrkshp.setDescription(desc.toString()));
+		
+		OWLObjectProperty hasProgression = dataFact.getOWLObjectProperty("http://purl.org/cwmo/#hasProgression");
+		Stream<OWLNamedIndividual> wrkshpStage = reasoner.getObjectPropertyValues(workshopInstance, hasProgression)
+				.entities();
+		wrkshpStage.forEach(phase -> wrkshp.setStage(phase.getIRI().getShortForm()));
+		
+		OWLObjectProperty hasStrategy = dataFact.getOWLObjectProperty("http://purl.org/cwmo/#isImplementingStrat");
+		Stream<OWLNamedIndividual> wrkshpStrategy = reasoner.getObjectPropertyValues(workshopInstance, hasStrategy)
+				.entities();
+		wrkshpStrategy.forEach(strat -> wrkshp.setStage(strat.getIRI().getShortForm()));
+		
+		return wrkshp;
+		
+	}
+	
+	/**
+	 * Generate a list of workshop object based on all the workshops available in the ontology
+	 * @return list of workshop
+	 */
+	public ArrayList<Workshop> getWorkshops(){
+		ArrayList<Workshop> wrkshpList = new ArrayList<Workshop>();
+		
+		OWLReasonerFactory reasonerFactory = new StructuralReasonerFactory();
+		OWLReasoner reasoner = reasonerFactory.createReasoner(onto);
+		
+		IRI creatWorkshop = IRI.create("http://purl.org/cwmo/#CreativeWorkshop");
+		OWLDataFactory dataFact = man.getOWLDataFactory();
+		OWLClass CreatWrkshpClass = dataFact.getOWLClass(creatWorkshop);
+
+		NodeSet<OWLNamedIndividual> individualsNodeSet = reasoner.getInstances(CreatWrkshpClass, true);
+		Stream<OWLNamedIndividual> individuals = individualsNodeSet.entities();
+
+		individuals.forEach(x -> wrkshpList.add(getWorkshop(x.getIRI().getShortForm())));
+		
+		return wrkshpList;
+		
+	}
+	
+	/**
+	 * Set the information concerning a workshop in the ontology
+	 * @param wrkshp 
+	 */
+	public void setWorkshop(Workshop wrkshp) {
+		
+	}
 	
 
 }
